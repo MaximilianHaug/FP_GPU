a0eb8d1a76b61fb79f90e15033c6f44e634d7736
MaximilianHaug <st182808@stud.uni-stuttgart.de>
2026-03-24 11:23:19 +0100

    qiskit teil 1

---
+++ playground/qiskit_1.py
@@ -0,0 +1,17 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+
+bits = 3
+qreg_q = QuantumRegister(bits + 1, "q")
+creg_c = ClassicalRegister(bits, "c")
+circuit = QuantumCircuit(qreg_q, creg_c)
+
+
+circuit.h(qreg_q[0])
+circuit.cx(qreg_q[0], qreg_q[1])
+circuit.cx(qreg_q[1], qreg_q[2])
+circuit.cx(qreg_q[2], qreg_q[3])
+
+
+circuit.measure(range(bits), range(bits))
+
+circuit.draw(output="mpl", idle_wires=False, style="iqp")
